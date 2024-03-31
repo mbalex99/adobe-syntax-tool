2fc27300f580db547091bdf2e77ba9128a344bc5
Maximilian Alexander <max@ditto.live>
2024-03-30 22:53:45 -0700

    disable for non-supported products

--- src/jsx/ppro/ppro.ts
+++ src/jsx/ppro/ppro.ts
@@ -8,6 +8,12 @@ import {
 } from "../utils/samples";
 export { helloError, helloStr, helloNum, helloArrayStr, helloObj, helloVoid };
 import { dispatchTS } from "../utils/utils";
+import {
+  fillMogrtText,
+  forEachChild,
+  forEachClip,
+  forEachVideoTrack,
+} from "./ppro-utils";
 
 export const qeDomFunction = () => {
   if (typeof qe === "undefined") {
@@ -21,4 +27,24 @@ export const qeDomFunction = () => {
 
 export const helloWorld = () => {
   alert("Hello from Premiere Pro.");
+};
+
+export const getSelectedTextLayers = () => {
+  const activeSequence = app.project.activeSequence;
+  if (!activeSequence) {
+    return [];
+  }
+  const videoTracks = activeSequence.videoTracks;
+
+  var mogrtClip: TrackItem | null = null;
+  for (var i = 0; i < videoTracks.numTracks; i++) {
+    var track = videoTracks[i];
+    for (var j = 0; j < track.clips.numItems; j++) {
+      var clip = track.clips[j];
+      if (clip.isSelected()) {
+        mogrtClip = clip
+        break;
+      }
+    }
+  }
 };
